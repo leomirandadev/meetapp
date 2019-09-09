8cfe93e937c98a7ddb99075698f2b63350225c2b
Leonardo Miranda <leoamiranda2@gmail.com>
2019-09-09 09:44:17 -0300

    função update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -38,6 +38,57 @@ class UserController {
       },
     });
   }
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      id: Yup.number()
+        .integer()
+        .required(),
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        message: 'Validation fails',
+      });
+    }
+
+    const { id, email, oldPassword } = req.body;
+    const user = await User.findByPk(id);
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({
+          message: 'E-mail already exists',
+        });
+      }
+    }
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({
+        message: 'Password does not match',
+      });
+    }
+
+    const { name } = await user.update(req.body);
+    return res.json({
+      message: 'Updated was success',
+      output: {
+        id,
+        name,
+        email,
+      },
+    });
+  }
 
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import MiddlewareAuth from './app/middlewares/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.put('/users', MiddlewareAuth, UserController.update);
 routes.post('/sessions', SessionController.store);
 
 export default routes;
